03554d4f72e6cad4c8c5d9ba39beb5055f0e41bb
Ancientshield <jacklin2100@gmail.com>
2023-08-09 21:13:10 +0800

    num have no ref

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <div></div>
+	<div>
+		<p>{{ num }}</p>
+		<button @click.prevent="increment">Click Me</button>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "App",
-};
+	export default {
+		name: 'App',
+		setup() {
+			let num = 0;
+
+			const increment = () => {
+				num++;
+			};
+
+			return {
+				num,
+				increment,
+			};
+		},
+	};
 </script>
