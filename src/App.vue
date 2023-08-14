1957176e1170921df029f4891850b10e10d30115
Ancientshield <jacklin2100@gmail.com>
2023-08-14 22:42:59 +0800

    use <script setup> syntax sugar

--- src/App.vue
+++ src/App.vue
@@ -17,55 +17,35 @@
 	</div>
 </template>
 
-<script>
+<script setup>
 	import { ref, reactive, toRefs, onBeforeMount, onMounted } from 'vue';
 
 	import AppAlert from './components/Alert.vue';
 	import { useNumber } from './hooks/number.js';
 	import { usePhrase } from './hooks/phrase.js';
 
-	export default {
-		name: 'App',
-		// 記得加s
-		components: {
-			// 不用 return
-			AppAlert,
-		},
-		setup() {
-			const btn = ref(null);
-			onBeforeMount(() => {
-				console.log('onBeforeMount()');
-			});
+	const btn = ref(null);
+	onBeforeMount(() => {
+		console.log('onBeforeMount()');
+	});
 
-			onMounted(() => {
-				btn.value.addEventListener('click', () => {
-					console.log('button clicked');
-				});
-			});
+	onMounted(() => {
+		btn.value.addEventListener('click', () => {
+			console.log('button clicked');
+		});
+	});
 
-			const user = reactive({
-				name: 'John',
-				age: 20,
-			});
+	const user = reactive({
+		name: 'John',
+		age: 20,
+	});
 
-			setTimeout(() => {
-				user.name = 'Luis';
-			}, 3000);
+	setTimeout(() => {
+		user.name = 'Luis';
+	}, 3000);
 
-			const { num, increment, double } = useNumber();
-			const { phrase, reversePhrase, num: phraseNum } = usePhrase();
+	const { num, increment, double } = useNumber();
+	const { phrase, reversePhrase, num: phraseNum } = usePhrase();
 
-			return {
-				num,
-				increment,
-				...toRefs(user),
-				phrase,
-				reversePhrase,
-				double,
-				user,
-				btn,
-				phraseNum,
-			};
-		},
-	};
+	const { name } = toRefs(user);
 </script>
